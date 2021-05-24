39bf7df1c8239c26c2ea8f9d9331deed021a402e
ungarmichael <ungarmichael.mail@gmail.com>
2021-05-24 13:38:28 +0200

    ok

--- pages/Projects/index.jsx
+++ pages/Projects/index.jsx
@@ -25,7 +25,7 @@ const Projects = () => {
       Title: "Neighboroo",
       Slogan: "Bringing Neighborhoods together.",
       Description:
-        "Neighboroo is a iOS and Android-application which is built for getting in touch with your neighbors because especially during these hard times this can be quite hard. The whole system is divided into four categories: Messenger, Selling, Transport, and Service. In the Messenger you are able to chat with your neighbors. The Trading section is for selling goods. Car-sharing can be done in the Transport section and the service section is for Offering and receiving simple jobs.",
+        "Neighboroo is a iOS and Android-application which is built for getting in touch with your neighbors because especially during these hard times this can be quite complicated. The whole system is divided into four categories: Messenger, Selling, Transport, and Service. In the Messenger you are able to chat with your neighbors. The Trading section is for selling goods. Car-sharing can be done in the Transport section and the service section is for Offering and receiving simple jobs.[2020]",
       Link: "Link to the Github Project",
       Image: "/assets/Neighboroo/NeighborooMockup.png",
       LinkURL: "https://github.com/ungarmichael/neighboroo",
